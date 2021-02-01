1dc71a142c75d7da85c7a1d754907e3252521cd4
krishnachaitanya-learn <krishnachaitanyayerrapatruni@gmail.com>
2021-02-01 17:58:57 +0530

    youtube 29:25
    from JSMASTERY tutorial in youtube

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ChatEngine } from "react-chat-engine";
 
 import "./App.css";
+import "../components/ChatFeed";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       projectID="5419d4cc-92d9-4e4c-bb5e-d5a673ff09f5"
       userName="arjun"
       userSecret="arjun"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
